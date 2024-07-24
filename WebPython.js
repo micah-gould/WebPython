0c7b54381618884c1e62f298b9efddfd4bb946a0
micah-gould <Gouldmicah@gmail.com>
2024-07-24 14:08:47 +0200

    Added Loading

--- WebPython.js
+++ WebPython.js
@@ -1,20 +1,33 @@
 window.onload = async () => {
+  document.getElementById('output').value = 'Pyodide loading'
   const START = Date.now()
+  // Load Pyodide
   const pyodide = await loadPyodide({
     indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/'
   })
+  // Setup Pyodide output
+  pyodide.runPython(`
+  import sys
+  import io
+  sys.stdout = io.StringIO()
+  `)
   const END = Date.now()
-  document.getElementById('output').value = `Pyodide loaded in ${END - START}ms`
-  document.getElementById('run').onclick = async () => {
-    document.getElementById('output').value = ''
-    const code = document.getElementById('input').value
-    pyodide.runPython(`
-    import sys
-    import io
-    sys.stdout = io.StringIO()
-    `)
-    pyodide.runPython(code)
-    const stdout = pyodide.runPython('sys.stdout.getvalue()')
-    document.getElementById('output').value = stdout
+  document.getElementById('output').value += `\nPyodide loaded in ${END - START}ms`
+
+  document.getElementById('test').onclick = () => {
+    document.getElementById('output').value = '' // Clear output
+    const code = document.getElementById('input').value // Get python code
+    pyodide.runPython(code) // Run python
+    const stdout = pyodide.runPython('sys.stdout.getvalue()') // Get output
+    document.getElementById('output').value = stdout // Display output
+  }
+
+  document.getElementById('load').onclick = () => {
+    document.getElementById('input').value = Object.values(setup.requiredfiles) // Get code from setup object
+    // fetch('')
+    //   .then(response => response.text())
+    //   .then((data) => {
+    //     document.getElementById('input').value = data
+    //   })
   }
 }
