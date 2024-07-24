06d0b2d0afd8b0c74d0560870a827cd9f2e35954
micah-gould <Gouldmicah@gmail.com>
2024-07-24 17:09:57 +0200

    Added checking and scoring of code

--- WebPython.js
+++ WebPython.js
@@ -1,5 +1,7 @@
 window.onload = async () => {
-  document.getElementById('output').value = 'Pyodide loading'
+  const OUTPUT = document.getElementById('output')
+  const INPUT = document.getElementById('input')
+  OUTPUT.value = 'Pyodide loading'
   const START = Date.now()
   // Load Pyodide
   const pyodide = await loadPyodide({
@@ -12,22 +14,45 @@ window.onload = async () => {
   sys.stdout = io.StringIO()
   `)
   const END = Date.now()
-  document.getElementById('output').value += `\nPyodide loaded in ${END - START}ms`
+  OUTPUT.value += `\nPyodide loaded in ${END - START}ms`
 
   document.getElementById('test').onclick = () => {
-    document.getElementById('output').value = '' // Clear output
-    const code = document.getElementById('input').value // Get python code
+    OUTPUT.value = '' // Clear output
+    const code = INPUT.value // Get python code
     pyodide.runPython(code) // Run python
-    const stdout = pyodide.runPython('sys.stdout.getvalue()') // Get output
-    document.getElementById('output').value = stdout // Display output
+    const stdout = pyodide.runPython('sys.stdout.getvalue()') // Get output. FIXME: Currently gets all the outputs since loading
+    OUTPUT.value = stdout // Display output
   }
 
   document.getElementById('load').onclick = () => {
-    document.getElementById('input').value = Object.values(setup.requiredfiles) // Get code from setup object
+    INPUT.value = Object.values(setup.requiredfiles) // Get code from setup object
     // fetch('')
     //   .then(response => response.text())
     //   .then((data) => {
-    //     document.getElementById('input').value = data
+    //     INPUT.value = data
     //   })
+  }
+
+  document.getElementById('run').onclick = () => {
+    OUTPUT.value = '' // Clear output
+    const code = INPUT.value // Get python code
+    pyodide.runPython(code) // Run python
+    const func = code.split('\n')[0].split(' ')[1].split('(')[0] // Get function name
+    const total = setup.inputs.length
+    let correct = 0
+    let incorrect = 0
+    for (let i = 0; i < total; i++) {
+      pyodide.runPython(`print(${func}('${setup.inputs[i]}'))`) // Run each testcase
+      const stdout = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(-2)[0] // Get output
+      OUTPUT.value += stdout // Display output
+      if (stdout === setup.outputs[i]) { // Check if output was correct
+        correct++
+        OUTPUT.value += ` CORRECT, Score: ${(correct / (i + 1)) * 100}%\n`
+      } else {
+        incorrect++
+        OUTPUT.value += ` CORRECT, Score: ${(incorrect / (i + 1)) * 100}%\n`
+      }
+    }
+    OUTPUT.value += `Correct: ${correct}, Incorrect: ${incorrect}, Score: ${(correct / total) * 100}%` // Final score
   }
 }
