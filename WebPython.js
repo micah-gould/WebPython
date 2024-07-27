e1f7d594c191df2758303497f8c7cad3046a4276
micah-gould <Gouldmicah@gmail.com>
2024-07-28 00:02:53 +0200

    Added Error Handling

--- WebPython.js
+++ WebPython.js
@@ -6,7 +6,8 @@ window.addEventListener('load', async () => {
   const START = Date.now()
   // Load Pyodide
   const pyodide = await loadPyodide({
-    indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/'
+    indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.20.0/full/',
+    stdout: (msg) => { OUTPUT.value += `\nPyodide: ${msg}` }
   })
   // data Pyodide output
   pyodide.runPython(`
@@ -22,10 +23,14 @@ window.addEventListener('load', async () => {
   document.getElementById('test').onclick = () => {
     OUTPUT.value = '' // Clear output
     const code = INPUT.value // Get python code
-    pyodide.runPython(code) // Run python
-    const stdout = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(stdoutOLD.length, -1).join('\n') // Get the new outputs
-    stdoutOLD = stdoutOLD.concat(stdout.split('\n')) // Add the new outputs to the list of old outputs
-    OUTPUT.value = stdout // Display output
+    try {
+      pyodide.runPython(code) // Run python
+      const stdout = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(stdoutOLD.length, -1).join('\n') // Get the new outputs
+      stdoutOLD = stdoutOLD.concat(stdout.split('\n')) // Add the new outputs to the list of old outputs
+      OUTPUT.value = stdout // Display output
+    } catch (err) {
+      OUTPUT.value = err.message
+    }
   }
 
   document.getElementById('load').onclick = () => {
@@ -45,31 +50,37 @@ window.addEventListener('load', async () => {
   document.getElementById('run').onclick = () => {
     OUTPUT.value = '' // Clear output
     const code = INPUT.value // Get python code
-    pyodide.runPython(code) // Run python
-
-    // This code fixes an issue if the user leaves in any print statemnts in the code
-    const stdout = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(stdoutOLD.length, -1).join('\n') // Get the new outputs
-    stdoutOLD = stdoutOLD.concat(stdout.split('\n')) // Add the new outputs to the list of old outputs
+    try {
+      pyodide.runPython(code) // Run python
+      // This code fixes an issue if the user leaves in any print statemnts in the code
+      const stdout = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(stdoutOLD.length, -1).join('\n') // Get the new outputs
+      stdoutOLD = stdoutOLD.concat(stdout.split('\n')) // Add the new outputs to the list of old outputs
+    } catch (err) {
+      OUTPUT.value = err
+    }
 
     const func = code.split('\n').filter(a => a.slice(0, 3) === 'def')[0].split(' ')[1].split('(')[0] // Get first function name
     const total = data.inputs.length
     let correct = 0
     let incorrect = 0
 
-    for (let i = 0; i < total; i++) {
-      pyodide.runPython(`print(${func}('${data.inputs[i]}'))`) // Run each testcase
-      const stdout = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(stdoutOLD.length, -1).join('\n') // Get the new outputs
-      stdoutOLD = stdoutOLD.concat(stdout.split('\n')) // Add the new outputs to the list of old outputs
-      OUTPUT.value += stdout // Display output
-      if (stdout === data.outputs[i]) { // Check if output was correct
-        correct++
-        OUTPUT.value += ` CORRECT, Score: ${(correct / (i + 1)) * 100}%\n`
-      } else {
-        incorrect++
-        OUTPUT.value += ` INCORRECT, Score: ${(correct / (i + 1)) * 100}%\n`
+    try {
+      for (let i = 0; i < total; i++) {
+        pyodide.runPython(`print(${func}('${data.inputs[i]}'))`) // Run each testcase
+        const stdout = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(stdoutOLD.length, -1).join('\n') // Get the new outputs
+        stdoutOLD = stdoutOLD.concat(stdout.split('\n')) // Add the new outputs to the list of old outputs
+        OUTPUT.value += stdout // Display output
+        if (stdout === data.outputs[i]) { // Check if output was correct
+          correct++
+          OUTPUT.value += ` CORRECT, Score: ${(correct / (i + 1)) * 100}%\n`
+        } else {
+          incorrect++
+          OUTPUT.value += ` INCORRECT, Score: ${(correct / (i + 1)) * 100}%\n`
+        }
       }
+      OUTPUT.value += `Correct: ${correct}, Incorrect: ${incorrect}, Score: ${(correct / total) * 100}%` // Final score
+    } catch (err) {
+      OUTPUT.value = err
     }
-
-    OUTPUT.value += `Correct: ${correct}, Incorrect: ${incorrect}, Score: ${(correct / total) * 100}%` // Final score
   }
 })
