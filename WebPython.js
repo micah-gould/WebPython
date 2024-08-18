5087483fb19a1e183d25fa7d5a7011f52a55bf3f
micah-gould <Gouldmicah@gmail.com>
2024-08-18 17:18:11 +0200

    Added comments

--- WebPython.js
+++ WebPython.js
@@ -4,49 +4,50 @@
     no-unused-vars is off because the function Python is written in this file but called from another */
 
 let stdoutOLD = [] // Array to store all past outputs (by line)
-let OUTPUT
-let pyodide
-let addText
-let setText
+let OUTPUT, pyodide, addText, setText // Variables that need to be global
+
 window.addEventListener('load', async () => {
-  addText = function (text, area) {
-    area.value += text
-    resize(area)
-  }
-  setText = function (text, area) {
-    area.value = text
-    resize(area)
-  }
-
-  document.getElementById('description').innerHTML += window.horstmann_codecheck.setup[0].description
-
-  OUTPUT = document.getElementById('output')
-  setText('Pyodide loading', OUTPUT)
-  const START = Date.now()
-  // Load Pyodide
-  pyodide = await loadPyodide({
-    indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.26.2/full/',
-    stdout: (msg) => { addText(`\nPyodide: ${msg}`, OUTPUT) },
-    stderr: (msg) => { addText(`${msg}\n`, OUTPUT) } // Unit Test raise the output as warning, so this redirects them to the output
-  })
-  // data Pyodide output
-  pyodide.runPython(`
-  import sys
-  import io
-  sys.stdout = io.StringIO()
-  `)
-  const END = Date.now()
-  addText(`\nPyodide loaded in ${END - START}ms`, OUTPUT)
-
-  function resize (area) {
+  function resize (area) { // Function to resize a text area
     // Reset the height to recalculate the correct scroll height
     area.style.height = 'auto'
     // Set the height to the scroll height to fit the content
     area.style.height = `${area.scrollHeight}px`
   }
+
+  addText = (text, area) => { // Function to add text to a text area and resize it
+    area.value += text
+    resize(area)
+  }
+  setText = (text, area) => { // Function to set the text of a text area and resize it
+    area.value = text
+    resize(area)
+  }
+
+  document.getElementById('description').innerHTML += window.horstmann_codecheck.setup[0].description // Set the description of the task
+  OUTPUT = document.getElementById('output') // Get the text area for the output
+
+  setText('Pyodide loading', OUTPUT) // Inform the user that Pyodide is loading
+  const START = Date.now() // Get the current time
+  // Load Pyodide
+  pyodide = await loadPyodide({
+    indexURL: 'https://cdn.jsdelivr.net/pyodide/v0.26.2/full/', // Make sure this is the correct version of pyodide
+    stdout: (msg) => { addText(`Pyodide: ${msg}\n`, OUTPUT) },
+    stderr: (msg) => { addText(`${msg}\n`, OUTPUT) } // Unit Test raise the output as warning, so this redirects them to the output
+  })
+  // Capture the Pyodide output
+  try {
+    pyodide.runPython('import sys\nimport io\nsys.stdout = io.StringIO()')
+  } catch (err) {
+    setText(err, OUTPUT)
+  }
+
+  const END = Date.now() // Get the current time
+  addText(`\nPyodide loaded in ${END - START}ms`, OUTPUT) // Inform the user that Pyodide has loaded
 })
 
+// Function that process the problems
 async function python (setup, params) {
+  // Generic HTML for every case
   let report = `<?xml version="1.0" encoding="utf-8"?>
   <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml">
@@ -68,33 +69,39 @@ async function python (setup, params) {
   <title>Report</title>
   </head>
   <body>`
+  // Itterate over each section
   for (let i = 0; i < setup.sections.length; i++) {
     OUTPUT.value = '' // Clear output
-    const name = Object.keys(setup.requiredFiles)[i]
+    const name = Object.keys(setup.requiredFiles)[i] // Get the name of the file
     const code = params[name] // Get python code
+    // Variables that are needed in every case
     let total = setup.sections[i]?.runs.length
-    let pf
-    let output
+    let pf, output
     let correct = 0
+
+    // Function to initialize the code
     function initialize (input) {
-      try { // Initialize the code
-        pyodide.runPython('print("")')
+      try {
+        pyodide.runPython('print("")') // TODO: idk why this needs to be here, but it breaks if I remove it, need to test it a bit
         pyodide.runPython(input) // Run python
-        // This code fixes an issue if the user leaves in any print statemnts in the code
-        const stdout = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(stdoutOLD.length, -1).join('\n') // Get the new outputs
-        stdoutOLD = stdoutOLD.concat(stdout.split('\n')) // Add the new outputs to the list of old outputs
+        // TODO: check if this is still necessary
+        getOutput()
       } catch (err) {
         setText(err, OUTPUT)
       }
     }
+
+    // Function to get the output of the python code
     function getOutput () {
       output = pyodide.runPython('sys.stdout.getvalue()').split('\n').slice(stdoutOLD.length, -1).join('\n') // Get the new outputs
       stdoutOLD = stdoutOLD.concat(output.split('\n')) // Add the new outputs to the list of old outputs
       addText(output + '\n', OUTPUT)
       return output
     }
+
+    // Function to compare the given output with the expected output and update all nessasary variables
     function check (expectedOutput, output, weight) {
-      total += weight - 1
+      total += weight - 1 // Used for the unit test to show the correct number of test, can be used if you want to weigh one input more than another
       if (expectedOutput !== output) { // Check if output was correct
         return 'fail'
       }
@@ -108,9 +115,11 @@ async function python (setup, params) {
         <table class="run">
         <tr><th>&#160;</th><th>Name</th><th>Arguments</th><th>Actual</th><th>Expected</th></tr>\n`
         initialize(code)
+        // Itterrate over the runs array
         for (let j = 0; j < setup.sections[i].runs.length; j++) {
           const func = setup.sections[i].runs[j].caption // Get function name
-          const input = setup.sections[i].runs[j].args[0].value
+          const input = setup.sections[i].runs[j].args[0].value // Get the inputs
+
           try {
             pyodide.runPython(`print(${func}(${input}))`) // Run each testcase
             pf = check(setup.sections[i].runs[j].output, getOutput(), 1)
@@ -143,19 +152,24 @@ async function python (setup, params) {
       case 'run':
         report += `<p class="header run">Running ${name}</p>
         <div class="run">`
+        // Itterrate over the runs array
         for (let j = 0; j < setup.sections[i].runs.length; j++) {
-          const inputs = setup.sections[i].runs[j].input.split('\n')
+          const inputs = setup.sections[i].runs[j].input.split('\n') // Get the inputs
+
           try {
             if (code.indexOf('input') !== -1) {
+              // Replace a user input with a computer input FIXME: only works when there is 1 input
               const str = 'next(inputs)'
               newCode = `inputs = iter([${inputs}])\n${code.replace(/input\((.*?)\)/, str)}` // Switch user input to computer input
               const index = newCode.indexOf('\n', newCode.indexOf(str) + str.length)
               const prompt = (str, start, end) => str.substring(str.indexOf(start) + start.length, str.indexOf(end, str.indexOf(start) + start.length))
               newCode = newCode.slice(0, index) + `${newCode.slice(index).match(/^\s*/)[0]}print(f"${prompt(code, 'input(', ')').slice(1, -1)}{${code.match(/(\b\w+\b)\s*=\s*.*?\binput\(/)[1]}}")` + newCode.slice(index) // Print the input question and inputed value
+
               pyodide.runPython(newCode) // Run each testcase
             } else {
-              initialize(code)
+              initialize(code) // Run each testcase
             }
+            // Run any other needed files
             if (setup.useFiles !== undefined) {
               for (file of Object.values(setup.useFiles)) {
                 newCode = file.replace(/from\s+\S+\s+import\s+\S+/g, '')
@@ -200,9 +214,11 @@ async function python (setup, params) {
           report += `<th>${arg.name}</th>`
         }
         report += '<th>Actual</th><th>Expected</th></tr>\n'
+        // Itterate over the runs array
         for (let j = 0; j < setup.sections[i].runs.length; j++) {
           try {
-            let newCode = code
+            let newCode = code // Copy the code
+            // Replace the variables with their new values
             for (arg of setup.sections[i].runs[j].args) {
               newCode = newCode.replace(new RegExp(`\\${arg.name}\\ .*`), `${arg.name} = ${arg.value}`)
             }
@@ -239,18 +255,22 @@ async function python (setup, params) {
       case 'unitTest':
         report += `<p class="header unitTest">Unit Tests</p>
         <div class="run">`
+        // Iterrate over runs array
         for (let j = 0; j < setup.sections[i].runs.length; j++) {
           try {
             initialize(code)
+            // Run any other needed files
             if (setup.useFiles !== undefined) {
-              const fileName = name.slice(0, -3)
+              const fileName = name.slice(0, -3) // Get the user's file's name
+              // Remove any importing of the user's file because it's functions were initialized
               newCode = setup.useFiles[setup.sections[i].runs[j].caption]
                 .replace(new RegExp(`from\\s+${fileName}\\s+import\\s+\\S+`, 'g'), '')
                 .replace(new RegExp(`^(import\\s+.*?)\\b${fileName.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\b(\\s*,)?`, 'gm'), (match, p1, p2, p3) =>
                   p1.replace(new RegExp(`\\b${fileName}\\b`), '').replace(/,\s*$/, '')
                 )
                 .replace(new RegExp(`\\b${fileName.replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}\\.`, 'g'), '') + '\ntry:\n  unittest.main()\nexcept SystemExit as e:\n  print(sys.stdout.getvalue())'
-              pyodide.runPython(newCode)
+
+              pyodide.runPython(newCode) // Run the unit tests
             }
             pf = check('OK', OUTPUT.value.split('\n')[4], parseInt(OUTPUT.value.split('\n')[2].match(/\d+/)[0]))
           } catch (err) {
