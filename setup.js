3e68fe1c3bbd47296337f3c8ae880a057fe17b5e
micah-gould <Gouldmicah@gmail.com>
2024-08-19 21:09:09 +0200

    Small Changes

--- setup.js
+++ setup.js
@@ -1,17 +1,32 @@
-window.addEventListener('load', () => {
-  const INPUT = document.getElementById('input')
+/* eslint no-unused-vars: off
+    -------------
+    no-unused-vars is off because the function updateInputs is written in this file but called from another
+ */
 
+let INPUTS
+let NAMES
+
+window.addEventListener('load', () => {
+  INPUTS = Array.from(document.getElementsByClassName('contents'))
+  NAMES = Array.from(document.getElementsByClassName('filename'))
   // Automatically update text area size
   document.addEventListener('keydown', () => {
-    INPUT.style.height = 'auto'
-    const fontSize = parseFloat(window.getComputedStyle(INPUT).fontSize)
-    INPUT.style.height = `${INPUT.scrollHeight + fontSize}px`
+    INPUTS.forEach(INPUT => {
+      INPUT.style.height = 'auto'
+      const fontSize = parseFloat(window.getComputedStyle(INPUT).fontSize)
+      INPUT.style.height = `${INPUT.scrollHeight + fontSize}px`
+    })
   })
 
-  document.getElementById('submit').addEventListener('click', () => { console.log(format(INPUT.value)) })
+  document.getElementById('submit').addEventListener('click', () => { INPUTS.forEach(INPUT => { console.log(format(INPUT.value)) }) })
 })
 
 function format (str) {
   // const specials = ['HIDE', 'EDIT', 'SUB', 'CALL']
   return str
+}
+
+function updateInputs () {
+  INPUTS = Array.from(document.getElementsByClassName('contents'))
+  NAMES = Array.from(document.getElementsByClassName('filename'))
 }
