43f38116ed0869f08fb4d4955b56550ad1019bca
micah-gould <Gouldmicah@gmail.com>
2024-10-15 20:24:08 +0200

    Reordered code and moved for loops for setup.sections[i].runs

--- WebPython.js
+++ WebPython.js
@@ -83,6 +83,208 @@ async function python (setup, params) {
     let correct = 0
     if (checkRequiredForbidden(code, setup.required, setup.forbiden)) break
 
+    // Iterrate over runs array
+    for (let j = 0; j < setup.sections[i].runs.length; j++) {
+      switch (setup.sections[i].type) { // TODO: work on test case 5
+        case 'call':
+          call()
+          break
+        case 'parsons':
+          parsons()
+          break
+        case 'run':
+          run()
+          break
+        case 'sub':
+          sub()
+          break
+        case 'unitTest':
+          unitTest()
+          break
+        case 'tester':
+          tester()
+          break
+      }
+    }
+
+    function checkRequiredForbidden (code, required, forbiden) {
+      setText('', OUTPUT)
+      let result = false
+      required?.forEach(test => {
+        if (!code.includes(Object.keys(test)[0])) {
+          addText(Object.values(test)[0], OUTPUT)
+          result = true
+        }
+      })
+      forbiden?.forEach(test => {
+        if (code.includes(Object.keys(test)[0])) {
+          addText(Object.values(test)[0], OUTPUT)
+          result = true
+        }
+      })
+      return result
+    }
+
+    function call () {
+      report += `<p class="header call">Calling with Arguments</p>
+      <div class="call">
+      <table class="run">
+      <tr><th>&#160;</th><th>Name</th><th>Arguments</th><th>Actual</th><th>Expected</th></tr>\n`
+      initialize(code)
+
+      const func = setup.sections[i].runs[j].caption // Get function name
+      const input = setup.sections[i].runs[j].args[0].value // Get the inputs
+      try {
+        pyodide.runPython(`print(${func}(${input}))`) // Run each testcase
+        const output = getOutput()
+        const expectedOutput = setup.sections[i].runs[j].output
+        pf = check(expectedOutput, output, 1)
+        report += `<tr><td><span class=${pf}>${pf}</span></td>
+            <td><pre>${name.split('.')[0]}</pre></td>
+            <td><pre>${input}</pre></td>
+            <td><pre>${output}
+            </pre></td>
+            <td><pre>${expectedOutput}
+            </pre></td>
+            </tr>\n`
+      } catch (err) {
+        setText(err, OUTPUT)
+      }
+
+      report += '</table>'
+    }
+
+    function parsons () {
+      report += `<p class="header run">Running ${name}</p>\n<div class="run">
+      <table class="run">
+      <tr><th>&#160;</th><th>Actual</th><th>Expected</th></tr>\n`
+
+      try {
+        pyodide.runPython(code)
+        const outputs = getOutput()
+        const expectedOutputs = setup.sections[i].runs[j].output
+        pf = check(expectedOutputs, outputs, 1)
+        report += `<tr><td><span class=${pf}>${pf}</span></td>
+            <td><pre>${outputs}
+            </pre></td>
+            <td><pre>${expectedOutputs}
+            </pre></td>
+            </tr>\n`
+      } catch (err) {
+        setText(err, OUTPUT)
+      }
+
+      report += '</table>'
+    }
+
+    function run () {
+      report += `<p class="header run">Running ${name}</p>\n<div class="run">
+      <table class="run">
+      <tr><th>&#160;</th><th>Actual</th><th>Expected</th></tr>\n`
+
+      const inputs = setup.sections[i].runs[j].input?.split('\n') // Get the inputs
+
+      try {
+        if (code.indexOf('input') !== -1) {
+          // Replace a user input with a computer input
+          const newStr = 'next(inputs)'
+          let newCode = `inputs = iter([${inputs}])\n${code}`
+          code.match(/input\((.*?)\)/g).forEach(str => {
+            const index = newCode.indexOf('\n', newCode.indexOf(str) + str.length)
+            const variable = newCode.match(/(\b\w+\b)\s*=\s*.*?\binput\(/)[1]
+            newCode = newCode.slice(0, index) + `${newCode.slice(index).match(/^\s*/)[0]}print(f"${str.slice(7, -2)}{${variable}}")` + newCode.slice(index) // Print the input question and inputed value
+            newCode = newCode.replace(/input\((.*?)\)/, newStr) // Switch user input to computer input
+          })
+
+          pyodide.runPython(newCode) // Run each testcase
+        } else {
+          initialize(code) // Run each testcase
+        }
+        useFiles()
+        const outputs = getOutput()
+        const expectedOutputs = setup.sections[i].runs[j].output
+        pf = check(expectedOutputs, outputs, 1)
+        report += `<tr><td><span class=${pf}>${pf}</span></td>
+            <td><pre>${outputs}
+            </pre></td>
+            <td><pre>${expectedOutputs}
+            </pre></td>
+            </tr>\n`
+      } catch (err) {
+        setText(err, OUTPUT)
+      }
+
+      report += '</table>'
+    }
+
+    function sub () {
+      report += `<p class="header sub">Running program with substitutions</p>
+      <div class="sub">
+      <table class="run">
+      <tr><th>&#160;</th><th>Name</th>`
+      for (arg of setup.sections[i].runs[0].args) {
+        report += `<th>${arg.name}</th>`
+      }
+      report += '<th>Actual</th><th>Expected</th></tr>\n'
+
+      try {
+        let newCode = code // Copy the code
+        // Replace the variables with their new values
+        for (arg of setup.sections[i].runs[j].args) {
+          newCode = newCode.replace(new RegExp(`\\${arg.name}\\ .*`), `${arg.name} = ${arg.value}`)
+        }
+        pyodide.runPython(newCode) // Run each testcase
+        const output = getOutput()
+        const expectedOutput = setup.sections[i].runs[j].output
+        pf = check(expectedOutput, output, 1)
+        report += `<tr><td><span class=${pf}>${pf}</span></td>
+            <td><pre>${name.split('.')[0]}</pre></td>`
+        for (arg of setup.sections[i].runs[j].args) {
+          report += `<td><pre>${arg.value}</pre></td>`
+        }
+        report += `<td><pre>${output}\n</pre></td>\n<td><pre>${expectedOutput}\n</pre></td>\n</tr>\n`
+      } catch (err) {
+        setText(err, OUTPUT)
+      }
+
+      report += '</table>'
+    }
+
+    function unitTest () { // FIXME: Breaks if use was wrong
+      report += '<p class="header unitTest">Unit Tests</p>\n<div class="run">'
+
+      try {
+        initialize(code)
+        useFiles(true)
+        pf = check('OK', OUTPUT.value.split('\n')[4], parseInt(OUTPUT.value.split('\n')[2].match(/\d+/)[0]))
+      } catch (err) {
+        setText(err, OUTPUT)
+      }
+
+      report += `<span class=${pf}>${pf}</span>`
+    }
+
+    function tester () {
+      report += '<p class="header tester">Testers</p>\n<div class="run">'
+
+      try {
+        initialize(code)
+        useFiles()
+        let HTMLoutput = '<pre class=\'output\'>'
+        const expectedOutputs = setup.sections[i].runs[j].output.split('\n').filter(n => n)
+        const outputs = getOutput().split('\n')
+        for (let k = 0; k < expectedOutputs.length; k++) {
+          pf = check(expectedOutputs[k], outputs[k], 1)
+          HTMLoutput += `${outputs[k]}\n<span class=${pf}>${expectedOutputs[++k]}</span>\n`
+          report += `<span class=${pf}>${pf}</span> `
+        }
+        total = outputs.length / 2
+        report += HTMLoutput
+      } catch (err) {
+        setText(err, OUTPUT)
+      }
+    }
+
     // Function to initialize the code
     function initialize (input) {
       try {
@@ -138,203 +340,7 @@ async function python (setup, params) {
         pyodide.runPython(newCode)
       }
     }
-    function parsons () {
-      report += `<p class="header run">Running ${name}</p>\n<div class="run">
-      <table class="run">
-      <tr><th>&#160;</th><th>Actual</th><th>Expected</th></tr>\n`
-      for (let j = 0; j < setup.sections[i].runs.length; j++) {
-        try {
-          pyodide.runPython(code)
-          const outputs = getOutput()
-          const expectedOutputs = setup.sections[i].runs[j].output
-          pf = check(expectedOutputs, outputs, 1)
-          report += `<tr><td><span class=${pf}>${pf}</span></td>
-            <td><pre>${outputs}
-            </pre></td>
-            <td><pre>${expectedOutputs}
-            </pre></td>
-            </tr>\n`
-        } catch (err) {
-          setText(err, OUTPUT)
-        }
-      }
-      report += '</table>'
-    }
-    function run () {
-      report += `<p class="header run">Running ${name}</p>\n<div class="run">
-      <table class="run">
-      <tr><th>&#160;</th><th>Actual</th><th>Expected</th></tr>\n`
-      // Itterrate over the runs array
-      for (let j = 0; j < setup.sections[i].runs.length; j++) {
-        const inputs = setup.sections[i].runs[j].input?.split('\n') // Get the inputs
 
-        try {
-          if (code.indexOf('input') !== -1) {
-          // Replace a user input with a computer input
-            const newStr = 'next(inputs)'
-            let newCode = `inputs = iter([${inputs}])\n${code}`
-            code.match(/input\((.*?)\)/g).forEach(str => {
-              const index = newCode.indexOf('\n', newCode.indexOf(str) + str.length)
-              const variable = newCode.match(/(\b\w+\b)\s*=\s*.*?\binput\(/)[1]
-              newCode = newCode.slice(0, index) + `${newCode.slice(index).match(/^\s*/)[0]}print(f"${str.slice(7, -2)}{${variable}}")` + newCode.slice(index) // Print the input question and inputed value
-              newCode = newCode.replace(/input\((.*?)\)/, newStr) // Switch user input to computer input
-            })
-
-            pyodide.runPython(newCode) // Run each testcase
-          } else {
-            initialize(code) // Run each testcase
-          }
-          useFiles()
-          const outputs = getOutput()
-          const expectedOutputs = setup.sections[i].runs[j].output
-          pf = check(expectedOutputs, outputs, 1)
-          report += `<tr><td><span class=${pf}>${pf}</span></td>
-            <td><pre>${outputs}
-            </pre></td>
-            <td><pre>${expectedOutputs}
-            </pre></td>
-            </tr>\n`
-        } catch (err) {
-          setText(err, OUTPUT)
-        }
-      }
-      report += '</table>'
-    }
-    function call () {
-      report += `<p class="header call">Calling with Arguments</p>
-      <div class="call">
-      <table class="run">
-      <tr><th>&#160;</th><th>Name</th><th>Arguments</th><th>Actual</th><th>Expected</th></tr>\n`
-      initialize(code)
-      // Itterrate over the runs array
-      for (let j = 0; j < setup.sections[i].runs.length; j++) {
-        const func = setup.sections[i].runs[j].caption // Get function name
-        const input = setup.sections[i].runs[j].args[0].value // Get the inputs
-        try {
-          pyodide.runPython(`print(${func}(${input}))`) // Run each testcase
-          const output = getOutput()
-          const expectedOutput = setup.sections[i].runs[j].output
-          pf = check(expectedOutput, output, 1)
-          report += `<tr><td><span class=${pf}>${pf}</span></td>
-            <td><pre>${name.split('.')[0]}</pre></td>
-            <td><pre>${input}</pre></td>
-            <td><pre>${output}
-            </pre></td>
-            <td><pre>${expectedOutput}
-            </pre></td>
-            </tr>\n`
-        } catch (err) {
-          setText(err, OUTPUT)
-        }
-      }
-      report += '</table>'
-    }
-    function sub () {
-      report += `<p class="header sub">Running program with substitutions</p>
-      <div class="sub">
-      <table class="run">
-      <tr><th>&#160;</th><th>Name</th>`
-      for (arg of setup.sections[i].runs[0].args) {
-        report += `<th>${arg.name}</th>`
-      }
-      report += '<th>Actual</th><th>Expected</th></tr>\n'
-      // Itterate over the runs array
-      for (let j = 0; j < setup.sections[i].runs.length; j++) {
-        try {
-          let newCode = code // Copy the code
-          // Replace the variables with their new values
-          for (arg of setup.sections[i].runs[j].args) {
-            newCode = newCode.replace(new RegExp(`\\${arg.name}\\ .*`), `${arg.name} = ${arg.value}`)
-          }
-          pyodide.runPython(newCode) // Run each testcase
-          const output = getOutput()
-          const expectedOutput = setup.sections[i].runs[j].output
-          pf = check(expectedOutput, output, 1)
-          report += `<tr><td><span class=${pf}>${pf}</span></td>
-            <td><pre>${name.split('.')[0]}</pre></td>`
-          for (arg of setup.sections[i].runs[j].args) {
-            report += `<td><pre>${arg.value}</pre></td>`
-          }
-          report += `<td><pre>${output}\n</pre></td>\n<td><pre>${expectedOutput}\n</pre></td>\n</tr>\n`
-        } catch (err) {
-          setText(err, OUTPUT)
-        }
-      }
-      report += '</table>'
-    }
-    function unitTest () { // FIXME: Breaks if use was wrong
-      report += '<p class="header unitTest">Unit Tests</p>\n<div class="run">'
-      // Iterrate over runs array
-      for (let j = 0; j < setup.sections[i].runs.length; j++) {
-        try {
-          initialize(code)
-          useFiles(true)
-          pf = check('OK', OUTPUT.value.split('\n')[4], parseInt(OUTPUT.value.split('\n')[2].match(/\d+/)[0]))
-        } catch (err) {
-          setText(err, OUTPUT)
-        }
-      }
-      report += `<span class=${pf}>${pf}</span>`
-    }
-    function tester () {
-      report += '<p class="header tester">Testers</p>\n<div class="run">'
-      // Iterrate over runs array
-      for (let j = 0; j < setup.sections[i].runs.length; j++) {
-        try {
-          initialize(code)
-          useFiles()
-          let HTMLoutput = '<pre class=\'output\'>'
-          const expectedOutputs = setup.sections[i].runs[j].output.split('\n').filter(n => n)
-          const outputs = getOutput().split('\n')
-          for (let k = 0; k < expectedOutputs.length; k++) {
-            pf = check(expectedOutputs[k], outputs[k], 1)
-            HTMLoutput += `${outputs[k]}\n<span class=${pf}>${expectedOutputs[++k]}</span>\n`
-            report += `<span class=${pf}>${pf}</span> `
-          }
-          total = outputs.length / 2
-          report += HTMLoutput
-        } catch (err) {
-          setText(err, OUTPUT)
-        }
-      }
-    }
-    function checkRequiredForbidden (code, required, forbiden) {
-      setText('', OUTPUT)
-      let result = false
-      required?.forEach(test => {
-        if (!code.includes(Object.keys(test)[0])) {
-          addText(Object.values(test)[0], OUTPUT)
-          result = true
-        }
-      })
-      forbiden?.forEach(test => {
-        if (code.includes(Object.keys(test)[0])) {
-          addText(Object.values(test)[0], OUTPUT)
-          result = true
-        }
-      })
-      return result
-    }
-    switch (setup.sections[i].type) { // TODO: work on test case 5
-      case 'call':
-        call()
-        break
-      case 'parsons':
-        parsons()
-        break
-      case 'run':
-        run()
-        break
-      case 'sub':
-        sub()
-        break
-      case 'unitTest':
-        unitTest()
-        break
-      case 'tester':
-        tester()
-        break
-    }
     report += `</div>
     <p class="header studentFiles">Submitted files</p>
     <div class="studentFiles">
